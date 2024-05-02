6b33595e3ab5cef59ee609904d6dbde1014d6129
Max Zyong <phenom-max@yandex.ru>
2024-05-03 00:22:57 +0300

    test husky

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
-import React from 'react';
-import Step from './Step';
-import './App.css';
+import React from "react";
+import Step from "./Step";
+import "./App.css";
 
 function App() {
   return (
-    <div className='App'>
+    <div className="App">
       <Step />
     </div>
-    
-  )
+  );
 }
 
 export default App;
